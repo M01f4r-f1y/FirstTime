62852cf99547d6445bb6ff312057affd0915801a
M01f4r-f1ly <pill.happyness@gmail.com>
2026-09-19 17:41:22 +0300

    Create name\age

--- L5.py
+++ L5.py
@@ -1,4 +1,9 @@
 for _ in range(42):
 	print("I love python")
 
+age_in_month = (2026-2001)+9
+age_in_yers = age_in_month//12
+my_name = "Artem"
+My_age = "My name is {my_name}, I am {age_in_yers} years old."
+
 
