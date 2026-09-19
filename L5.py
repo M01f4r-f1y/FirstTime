35bf813f417b6e21eebcc5f65eaa17691f507077
M01f4r-f1ly <pill.happyness@gmail.com>
2026-09-19 17:52:04 +0300

    Fix 1

--- L5.py
+++ L5.py
@@ -1,16 +1,34 @@
+space = 0
 for _ in range(42):
 	print("I love python")
+
+for _ in range(2):
+	print(space)
 
 age_in_month = (2026-2001)+9
 age_in_yers = age_in_month//12
 my_name = "Artem"
 My_age = "My name is {my_name}, I am {age_in_yers} years old."
+print(My_age)
 
-a = 1
-print (a == 1)
-print (a != 2)
-print(a > 3)
-print(a < 4)
-print(a <= 5)
+for _ in range(2):
+	print(space)
+
+n = 1
+print (n == 1)
+print (n != 2)
+print(n > 3)
+print(n < 4)
+print(n <= 5)
+
+for _ in range(2):
+	print(space)
+
+
+a = 2
+b = 5
+c = 6
+d = "{a}" + "{b}" + "{c}"
+print (d)
 
 
